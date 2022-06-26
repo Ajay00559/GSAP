b93636d1ddc0e10e7d5eabec6814c3dd635a6fff
AJAY KUMAR PATEL <patelajay00559@gmail.com>
2022-06-26 18:41:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+ gsap.from(".moving",{
+    top:"-300px",
+    left:"-250px",
+    duration:4,
+    delay:2,
+    ease:Expo.easeInOut
+ }) 
+
+//  gsap.to(".main",{
+//     top:"100%",
+//     left:"100%",
+//     duration:4,
+//     delay:2,
+//     ease:Expo.easeInOut
+//  }) 
